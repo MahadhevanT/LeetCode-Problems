01697f5dc6dc5729354cfcaeb17c9b7d9aff7311
MahadhevanT <137510110+MahadhevanT@users.noreply.github.com>
2024-05-29 13:34:25 +0530

    Time: 1 ms (75.47%) | Memory: 41.2 MB (82.46%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,25 +1,27 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-    String perfix=strs[0];
-    for(int i=1;i<strs.length;i++){
-        while(strs[i].indexOf(perfix)!=0){
-            perfix=perfix.substring(0,perfix.length()-1);
-        }
-    }
-    return perfix;
-    }
-}
-//     Arrays.sort(strs);
-//     String s1=strs[0];
-//     String s2=str[strs.length-1];
-//     int index=0;
-//     while(index<s1.length && index<s2.length){
-//         if(s1.charAt(index)==s2.charAt(index)){
-//             index++;
-//         }else{
-//             break;
+        //Method -1
+//     String perfix=strs[0];
+//     for(int i=1;i<strs.length;i++){
+//         while(strs[i].indexOf(perfix)!=0){
+//             perfix=perfix.substring(0,perfix.length()-1);
 //         }
-//     }  
-//     return substring(0,index);
+//     }
+//     return perfix;
 //     }
 // }
+//!!Method2
+    Arrays.sort(strs);
+    String s1=strs[0];
+    String s2=strs[strs.length-1];
+    int index=0;
+    while(index<s1.length() && index<s2.length()){
+        if(s1.charAt(index)==s2.charAt(index)){
+            index++;
+        }else{
+            break;
+        }
+    }  
+    return s1.substring(0,index);
+    }
+}
